dcb551b7b10963ed666e279ae9c08fd3c2475ee3
chris <christopheraburns@gmail.com>
2019-04-29 09:59:50 -0500

    added wsgi.py file

---
+++ container/yolo/wsgi.py
@@ -0,0 +1,6 @@
+import yolov3_app as myapp
+# This is just a simple wrapper for gunicorn to find your app.
+# If you want to change the algorithm file, simply change "predictor" above to the
+# new file.
+
+app = myapp.app
