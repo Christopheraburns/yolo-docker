d1ad75058eaa99f2fd0e0856daf8dbf5babdb09b
chris <christopheraburns@gmail.com>
2019-05-03 09:32:48 -0500

    upgrade to python 3.6 to match host

--- container/yolo/libCopy.py
+++ container/yolo/libCopy.py
@@ -19,7 +19,20 @@ with open("depend.csv", "r") as f:
 
 if INSTALL:
     # create directories if necessary and copy files (if they don't already exist)
-    stop=""
+
+    bodyparts = zip(pile_of_bodies, pile_of_heads)
+    for body, head in bodyparts:
+        # get just the directory of the file
+        path = body[: body.index("/", 10)]
+        if not os.path.isdir(path):
+            # create the dir
+            os.mkdir(path)
+
+        # Does the file already exist?
+        if not os.path.isfile(body):
+            # copy the file
+            copyfile("deleteMe/" + head, body)
+
 else:
     # Zip existing file
     if not os.path.isdir("bodyparts"):
